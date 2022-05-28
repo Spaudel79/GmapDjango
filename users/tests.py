4d5099b4c40daca98ada76c924e02fbf58e88029
Saroj Paudel <sarojpaudel79@gmail.com>
2022-05-28 18:58:23 -0400

    added a unit test for user

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class MyTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('test@gmail.com',
+                                             'Testman')
+
+    def test_user(self):
+        self.assertEqual(self.user.email, 'test@gmail.com')
+
+    def tearDown(self):
+        self.user.delete()
+
+
